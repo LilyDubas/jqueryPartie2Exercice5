9188c268e0d4aa811759915eba4cfcf26996d36b
Lily <nathdubas@gmail.com>
2019-11-04 21:28:00 +0100

    mouseenter mouseout

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  $('#lastName').mouseenter(function(){
+    $('#lastName').css("border","1px green solid");
+  $('#firstName').mouseenter(function(){
+    $('#firstName').css("border","1px green solid");
+  $('#firstName').mouseout(function(){
+    $('#firstName').css("border","1px red solid");
+  $('#lastName').mouseout(function(){
+    $('#lastName').css("border","1px red solid");
+    });
+  });
+  });
+  });
+  });
